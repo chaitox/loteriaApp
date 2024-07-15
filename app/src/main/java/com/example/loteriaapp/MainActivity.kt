5e215f1cff272e38f8fee10f6829738933433e4e
Ricardo Gomez <ricardo.jul.88@gmail.com>
2024-07-15 11:44:40 -0400

    control de estado con lista

--- app/src/main/java/com/example/loteriaapp/MainActivity.kt
+++ app/src/main/java/com/example/loteriaapp/MainActivity.kt
@@ -3,45 +3,35 @@ package com.example.loteriaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.loteriaapp.ui.theme.LoteriaAppTheme
+import com.example.loteriaapp.viewModels.ContadorViewModel
+import com.example.loteriaapp.viewModels.LoteriaViewModel
+import com.example.loteriaapp.views.Contador
+import com.example.loteriaapp.views.LoteriaView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        //val viewModel: ContadorViewModel by viewModels()
+        val viewModel: LoteriaViewModel by viewModels()
+
+        // enableEdgeToEdge()
         setContent {
             LoteriaAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoteriaView(viewModel)
+                    //Contador(viewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoteriaAppTheme {
-        Greeting("Android")
     }
 }
